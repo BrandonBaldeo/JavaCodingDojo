b7b02d7b1b0cb53ba7ddc3d14170c459562e031e
BrandonBaldeo <brandonb152@gmail.com>
2022-04-04 21:37:00 -0400

    init

---
+++ FirstJavaProgram.java
@@ -0,0 +1,11 @@
+/**
+ * FirstJavaProgram
+ */
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Brandon");
+        System.out.println("I am 24 years Old");
+        System.out.println("My hometown is Queens,NY");
+    }
+    
+}
